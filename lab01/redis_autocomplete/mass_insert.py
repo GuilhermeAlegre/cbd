cf84627165f5adffb313478b76d0a818bbfd0cf2
vascoalramos <vascoalramos@outlook.pt>
2019-09-27 01:15:44 +0100

    Added lab01 -> ex 1.4

---
+++ lab01/redis_autocomplete/mass_insert.py
@@ -0,0 +1,11 @@
+import redis
+
+# create the redis connection
+r = redis.Redis()
+
+# drop all data
+r.flushdb()
+
+with open("female-names.txt", "r") as reader:
+    for line in reader:
+        r.set(line.strip(), "")
